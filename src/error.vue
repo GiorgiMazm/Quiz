a5adc50f7a371c5435be397187ed7eaaf60405df
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2023-05-25 21:45:19 +0200

    error page

---
+++ src/error.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <AppHeader />
+    <div
+      class="text-center h-[80vh] text-8xl flex text-red-600 flex-col justify-center"
+    >
+      Upss 404... I can't found this page(
+      <p>Try to go to home page and try again</p>
+      <NuxtLink class="text-yellow-400 capitalize hover:text-yellow-600" to="/"
+        >go to home</NuxtLink
+      >
+    </div>
+    <AppFooter />
+  </div>
+</template>
+
+<style scoped></style>
