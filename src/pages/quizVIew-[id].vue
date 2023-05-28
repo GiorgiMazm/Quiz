3c2c165c45d669414b99e3ff46c0c14cdca387c1
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2023-05-28 15:50:36 +0200

    UI improvement for quiz vue page

--- src/pages/quizVIew-[id].vue
+++ src/pages/quizVIew-[id].vue
@@ -9,10 +9,14 @@ const pageLink = `/quiz-${route.params.id}/question-1`;
   <div>
     <section>
       <div class="container mx-auto">
-        <div v-if="quiz" class="flex justify-between w-2/5 mx-auto pt-9">
-          <h1 class="text-4xl font-bold">{{ quiz.name }}</h1>
-          <h2 class="text-2xl">{{ quiz.description }}</h2>
-          <NuxtLink @click="quiz.startQuiz()" :to="pageLink"
+        <div v-if="quiz" class="pt-9 text-cyan-800">
+          <h1 class="text-4xl font-bold">Quiz: {{ quiz.name }}</h1>
+          <h2 class="text-2xl py-4">Description: {{ quiz.description }}</h2>
+          <p class="text-xl">Quiz has {{ quiz.questionAmount }} questions</p>
+          <NuxtLink
+            class="py-3 px-4 bg-sky-400 mt-3 inline-block hover:bg-red-600 hover:text-black"
+            @click="quiz.startQuiz()"
+            :to="pageLink"
             >Start quiz</NuxtLink
           >
         </div>
