f2936ed7e5b94ba009035485984491fb6952b6bd
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2023-06-03 09:59:00 +0200

    create result page

--- src/error.vue
+++ src/error.vue
@@ -6,8 +6,7 @@
     <div
       class="text-center h-[80vh] text-8xl flex text-red-600 flex-col justify-center"
     >
-      Upss 404... I can't found this page(
-      <p>Try to go to home page and try again</p>
+      Upss 404... I can't found this page( Try again
       <NuxtLink class="text-yellow-400 capitalize hover:text-yellow-600" to="/"
         >go to home</NuxtLink
       >
---
+++ src/pages/result-[quizId].vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const route = useRoute();
+if (!route.query.correct) {
+  throw useError();
+}
+const { getQuizById } = useQuiz();
+const quiz = await getQuizById(+route.params.quizId);
+</script>
+
+<template>
+  <div>
+    <section>
+      <div class="container mx-auto text-center">
+        <h1 class="text-6xl mt-44">
+          Congratulation! You have just finished a quiz "{{ quiz.name }}"
+        </h1>
+        <h2 class="text-4xl">
+          You got {{ route.query.correct }} correct answers of
+          {{ quiz.questionAmount }}
+        </h2>
+        <NuxtLink
+          to="/quizzes"
+          class="bg-sky-600 py-4 px-7 rounded-xl mt-7 inline-block text-xl hover:bg-sky-400 cursor-pointer"
+          >Go to all quizzes</NuxtLink
+        >
+      </div>
+    </section>
+  </div>
+</template>
