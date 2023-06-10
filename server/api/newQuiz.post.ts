8e4af0ebb7acc77e70de4577ebd5a6a9e7a2d877
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2023-06-10 21:57:48 +0200

    post and delete routes

---
+++ server/api/newQuiz.post.ts
@@ -0,0 +1,13 @@
+import connectDb from "../plugins/connectDb";
+
+export default defineEventHandler(async (event) => {
+  const client = await connectDb();
+  if (!client) return;
+
+  const db = await client.db("quiz");
+  const quizzes = await db.collection("quizzes");
+
+  const body = await readBody(event);
+  await quizzes.insertOne(body);
+  return "success";
+});
