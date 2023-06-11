5b5b31f5c750028e1c3a97b37216d4fdeaba3a0e
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2023-06-11 07:03:55 +0200

    put route

---
+++ server/api/editQuiz.put.ts
@@ -0,0 +1,12 @@
+import connectDb from "../plugins/connectDb";
+
+export default defineEventHandler(async (event) => {
+  const client = await connectDb();
+  if (!client) return;
+  const db = await client.db("quiz");
+  const quizzes = await db.collection("quizzes");
+  const newQuiz = await readBody(event);
+
+  await quizzes.updateOne({ _id: newQuiz._id }, { $set: newQuiz });
+  return "success";
+});
