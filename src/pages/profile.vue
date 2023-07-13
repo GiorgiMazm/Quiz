eb1d1525664467d59854a974603cb03018d8b5f2
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2023-07-13 13:39:12 +0200

    show user's quizzes

--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -1,15 +1,48 @@
 <script setup lang="ts">
+import User from "~/types/User";
+import Quiz from "~/types/Quiz";
+import { Ref } from "vue";
+
 definePageMeta({ middleware: "auth" });
 const { getCurrentUser } = await useUser();
+const { getQuizById } = useQuiz();
+const user: User & { image: string } = Object.assign(
+  (await getCurrentUser()) as Object
+);
 
-const currentUser = await getCurrentUser();
+const quizzes: Ref<Quiz[]> = ref([]);
+onBeforeMount(async () => {
+  for (const quizID of user.quizzes) {
+    const quiz = await getQuizById(String(quizID));
+    quizzes.value.push(quiz);
+  }
+});
 </script>
 
 <template>
   <div>
     <section>
-      <div class="mx-auto container">user profile</div>
-      {{ currentUser }}
+      <div class="mx-auto container">
+        <h1 class="text-5xl text-center">Your Profile</h1>
+        <div>
+          <p v-if="user.image">
+            <img class="h-44" :src="user.image" alt="avatar" />
+          </p>
+          <p>Name: {{ user.name }}</p>
+          <p>Email: {{ user.email }}</p>
+
+          <div v-if="!user.image">
+            Your Quizzes:
+            <ul v-for="quiz in quizzes">
+              <li>
+                <NuxtLink :to="'/quizView-' + quiz?.id">{{
+                  quiz?.name
+                }}</NuxtLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </section>
   </div>
 </template>
