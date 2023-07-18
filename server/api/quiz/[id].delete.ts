55b47eded16cee40d678f502ec396bdb9b17c2d4
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2023-07-18 13:24:20 +0200

    delete quiz from user list after deleting it from quiz list

--- server/api/quiz/[id].delete.ts
+++ server/api/quiz/[id].delete.ts
@@ -1,11 +1,18 @@
 import connectDb from "../../plugins/connectDb";
 import { ObjectId } from "bson";
+import { PullOperator } from "mongodb";
 
 export default defineEventHandler(async (event) => {
   const client = await connectDb();
   if (!client) return;
   const db = await client.db("quiz");
   const quizzes = await db.collection("quizzes");
-  await quizzes.deleteOne({ _id: new ObjectId(event.context.params?.id) });
+  const users = await db.collection("users");
+  const quizObjectIdToRemove = new ObjectId(event.context.params?.id);
+  await users.updateMany(
+    { quizzes: { $in: [quizObjectIdToRemove] } },
+    { $pull: { quizzes: quizObjectIdToRemove } as PullOperator<Document> }
+  );
+  await quizzes.deleteOne({ _id: quizObjectIdToRemove });
   return "success";
 });
